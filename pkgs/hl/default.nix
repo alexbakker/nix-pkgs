c2948b27171a6bc70af00204436b8c0f61817471
Alexander Bakker <ab@alexbakker.me>
2024-03-28 23:38:42 +0100

    Update hl

--- pkgs/hl/default.nix
+++ pkgs/hl/default.nix
@@ -2,13 +2,13 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "hl";
-  version = "0.27.0";
+  version = "0.27.2";
 
   src = fetchFromGitHub {
     owner = "pamburus";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-XbzGHVICUuLTpSZcghhhWkYOMDZPeO/0rWmDzaguduU=";
+    hash = "sha256-VUkofFfSzXVFn5LmBnHGItf7r3q9ymMsUMJkmkmMoXE=";
   };
 
   cargoLock = {
